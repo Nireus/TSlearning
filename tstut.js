becbc702dc4ef623c826e813363cc7cc3c4481f1
Nireus <nir3us@gmail.com>
2017-09-17 13:35:45 +0200

    first lesson

---
+++ tstut.js
@@ -0,0 +1,19 @@
+var myName = "Jakub";
+var myAge = 41;
+var canVote = true;
+var anything;
+//anything = 2;
+document.getElementById("tsStuff").innerHTML = "My name is " + myName;
+document.write("myname is a " + typeof (myName) + "<br />");
+document.write("canVote is a " + typeof (canVote) + "<br />");
+document.write("myAge is a " + typeof (myAge) + "<br />");
+document.write("anything is a " + typeof (anything) + "<br /><br /><br />");
+var strToNum = parseInt("5");
+var numToStr = 6;
+document.write("numToStr is a " + typeof (numToStr.toString()) + "<br />");
+var PI = 3.14159;
+var superman = {
+    realName: "Clark Kent",
+    superName: "Superman"
+};
+document.write(superman.realName + " is " + superman.superName + "<br />");
---
+++ tstut.ts
@@ -0,0 +1,32 @@
+var myName: String = "Jakub";
+var myAge: number = 41;
+var canVote: boolean = true;
+var anything: any;
+//anything = 2;
+
+document.getElementById("tsStuff").innerHTML = "My name is " + myName;
+
+document.write("myname is a " + typeof (myName) + "<br />");
+document.write("canVote is a " + typeof (canVote) + "<br />");
+document.write("myAge is a " + typeof (myAge) + "<br />");
+document.write("anything is a " + typeof (anything) + "<br /><br /><br />");
+
+var strToNum: number = parseInt("5");
+var numToStr: number = 6;
+
+document.write("numToStr is a " + typeof (numToStr.toString()) + "<br />");
+
+const PI = 3.14159
+
+
+
+interface SuperHero {
+  realName: String;
+  superName: String;
+}
+
+var superman: SuperHero = {
+  realName: "Clark Kent",
+  superName: "Superman"
+}
+document.write(superman.realName + " is " + superman.superName + "<br />");
