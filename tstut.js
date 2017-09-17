e4bf05208b4f9864f0c46601a24c7765e83878d4
Nireus <nir3us@gmail.com>
2017-09-17 16:39:25 +0200

    fuctions

--- tstut.js
+++ tstut.js
@@ -20,3 +20,59 @@ document.write(superman.realName + " is " + superman.superName + "<br />");
 var employees = ["alice", "bob", "celine"];
 //employees.push(5);
 document.write(employees.toString() + "<br />");
+var superheroes = [];
+superheroes.push({
+    realName: "Bruce Wayne",
+    superName: "Batman"
+});
+superheroes.push(superman);
+document.write(superheroes[0].realName + " is a " + superheroes[0].superName + "<br /><br /><br />");
+document.write("5 + 4 = " + (5 + 4) + "<br />");
+var sampLet = 123;
+if (true) {
+    var sampLet_1 = 456;
+}
+document.write("sampLet : " + sampLet + "<br />");
+var sampVar = 123;
+if (true) {
+    var sampVar = 456;
+}
+document.write("sampVar : " + sampVar + "<br /><br />");
+var randArray = [5, 6, 7, 8];
+for (var val in randArray) {
+    document.write(val + "<br />");
+}
+document.write("<br />");
+var strArray = randArray.map(String);
+for (var _i = 0, strArray_1 = strArray; _i < strArray_1.length; _i++) {
+    var val = strArray_1[_i];
+    document.write(val + "<br />");
+}
+document.write("<br />");
+var getSum = function (num1, num2) {
+    return num1 + num2;
+};
+var theSum1 = getSum(5, 2);
+document.write("5 + 2 = " + theSum1 + "<br />");
+var getDiff = function (num1, num2, num3) {
+    if (num2 === void 0) { num2 = 2; }
+    if (typeof num3 !== 'undefined') {
+        return num1 - num2 - num3;
+    }
+    return num1 - num2;
+};
+document.write("5 - 2 = " + getDiff(5) + "<br />");
+document.write("5 - 3 = " + getDiff(5, 3) + "<br />");
+document.write("5 - 4 - 3 = " + getDiff(5, 4, 3) + "<br />");
+var sumAll = function () {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    var sum = nums.reduce(function (a, b) { return a + b; }, 0);
+    document.write("Sum : " + sum + "<br />");
+};
+sumAll(1, 2, 3, 4, 5, 6, 7);
+var addOne = function (x) { return x + 1; };
+document.write("1 + 1 = " + addOne(1) + "<br />");
+document.write("\"1\" + 1 = " + addOne("1") + "<br />");
--- tstut.ts
+++ tstut.ts
@@ -36,3 +36,76 @@ var employees: string[] = ["alice", "bob", "celine"];
 
 //employees.push(5);
 document.write(employees.toString() + "<br />")
+
+var superheroes: SuperHero[] = [];
+
+superheroes.push({
+    realName: "Bruce Wayne",
+    superName: "Batman"
+});
+superheroes.push(superman);
+
+document.write(superheroes[0].realName + " is a " + superheroes[0].superName + "<br /><br /><br />");
+
+
+document.write("5 + 4 = " + (5 + 4) + "<br />");
+
+let sampLet = 123;
+
+if (true) {
+    let sampLet = 456;
+}
+
+document.write("sampLet : " + sampLet + "<br />");
+
+var sampVar = 123;
+
+if (true) {
+    var sampVar = 456;
+}
+
+document.write("sampVar : " + sampVar + "<br /><br />");
+
+
+var randArray = [5, 6, 7, 8];
+
+for (var val in randArray) {
+    document.write(val + "<br />");
+}
+document.write("<br />");
+var strArray = randArray.map(String);
+
+for (var val of strArray) {
+    document.write(val + "<br />");
+}
+document.write("<br />");
+
+var getSum = function(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+var theSum1: number = getSum(5, 2);
+
+document.write("5 + 2 = " + theSum1 + "<br />");
+
+var getDiff = function(num1: number, num2 = 2, num3?: number): number {
+    if (typeof num3 !== 'undefined') {
+        return num1 - num2 - num3;
+    }
+    return num1 - num2;
+}
+
+document.write("5 - 2 = " + getDiff(5) + "<br />");
+document.write("5 - 3 = " + getDiff(5, 3) + "<br />");
+document.write("5 - 4 - 3 = " + getDiff(5, 4, 3) + "<br />");
+
+var sumAll = function(...nums: number[]): void {
+    var sum = nums.reduce((a, b) => a + b, 0);
+    document.write("Sum : " + sum + "<br />");
+}
+
+sumAll(1, 2, 3, 4, 5, 6, 7);
+
+var addOne = (x) => x + 1;
+document.write("1 + 1 = " + addOne(1) + "<br />");
+document.write("\"1\" + 1 = " + addOne("1") + "<br />");
